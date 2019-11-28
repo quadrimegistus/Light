da6a0a868993651c5470fbcbdf0055f2f7b74118
Wesley Hatley <whatley@lenovo.com>
2019-11-27 20:51:16 -0500

    Updates

---
+++ precache-manifest.bafaca0c855377a24d92e278fdac5533.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b65cf231316ec3085fb876f55f3da",
+    "url": "/Light/index.html"
+  },
+  {
+    "revision": "3b95f1528cf5305247bc",
+    "url": "/Light/static/css/2.b57565c3.chunk.css"
+  },
+  {
+    "revision": "db285a8ef796048568c0",
+    "url": "/Light/static/css/main.10c0986d.chunk.css"
+  },
+  {
+    "revision": "3b95f1528cf5305247bc",
+    "url": "/Light/static/js/2.330243b1.chunk.js"
+  },
+  {
+    "revision": "db285a8ef796048568c0",
+    "url": "/Light/static/js/main.3f554bee.chunk.js"
+  },
+  {
+    "revision": "a118bdb62a3de77ffd07",
+    "url": "/Light/static/js/runtime~main.b8a6b8b0.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/Light/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/Light/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/Light/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/Light/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "f8a971d4625e1cf4743f6049fbf1959c",
+    "url": "/Light/static/media/blog-1.f8a971d4.jpg"
+  },
+  {
+    "revision": "8e6cd66a043078a12d8d80b7532d1756",
+    "url": "/Light/static/media/blog-2.8e6cd66a.jpg"
+  },
+  {
+    "revision": "d7187400a97e4e3788a0c5aec7e20d66",
+    "url": "/Light/static/media/blog-3.d7187400.jpg"
+  },
+  {
+    "revision": "d43c772efc90236437dfb1df95c55758",
+    "url": "/Light/static/media/blog-4.d43c772e.jpg"
+  },
+  {
+    "revision": "b69dd3dc25235cec5f77307f4366b0bf",
+    "url": "/Light/static/media/blog-5.b69dd3dc.jpg"
+  },
+  {
+    "revision": "f653841fb3d4d64110a5fd512b2a6de5",
+    "url": "/Light/static/media/default-avatar.f653841f.png"
+  },
+  {
+    "revision": "3b3892d1bc708f01b084d960c7ba0ee7",
+    "url": "/Light/static/media/full-screen-image-1.3b3892d1.jpg"
+  },
+  {
+    "revision": "f7c47e22ad01fbf653496fd769b098cd",
+    "url": "/Light/static/media/full-screen-image-2.f7c47e22.jpg"
+  },
+  {
+    "revision": "ef9c8d65872c022182a94ffa984603d3",
+    "url": "/Light/static/media/full-screen-image-3.ef9c8d65.jpg"
+  },
+  {
+    "revision": "58666ce4494f218d25773a557ae57559",
+    "url": "/Light/static/media/full-screen-image-4.58666ce4.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Light/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Light/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Light/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Light/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Light/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Light/static/media/logo.ee7cd8ed.svg"
+  }
+]);
